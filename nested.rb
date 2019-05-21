0ece8ffc460f44e4121828760d238ceb255badd6
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 15:24:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+        programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+        programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+      string = programmer_hash[:dennis_ritchie][:languages]
+      string.join
 end
 
 def adding_matz
@@ -79,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    
     
 end
 

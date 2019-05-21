d1c9f8e67986e483e6965381a94f78015175f040
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 15:25:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    
+      adding_matz[:programmer_hash] = :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
     
 end
 
